7277fc1a6fc0a089c9bfec10ded8d49946b920c4
EvansOtieno <evansomondi@hotmail.com>
2020-12-16 00:52:27 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [jokes, setJoke] = useState({
+    joke: ''
+  });
+  const [categories, setCategory] = useState([]);
+
+  useEffect( () => {
+    fetchjoke();
+
+  }, []);
+
+  const fetchjoke = async () => {
+    const results = await axios.get('https://api.chucknorris.io/jokes/random');
+    const btns = await axios.get('https://api.chucknorris.io/jokes/categories');
+    setCategory(btns.data);
+    setJoke({
+      ...jokes,
+      joke: results.data.value
+    })
+  }
+
+  const resetjoke = async (cat) => {
+    console.log(cat.category);
+    const results = await axios.get(`https://api.chucknorris.io/jokes/random?category=${cat.category}`);
+    setJoke({
+      ...jokes,
+      joke: results.data.value
+    })
+  }
+  const handleClick = (cat, e) => {
+    e.preventDefault(); 
+    
+    resetjoke(cat);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="b">
+      <h2>Select Joke According To Buttons Bellow</h2>
+      <div class="bts">
+      {categories.map(category => {
+        return (<button onClick={(e) => handleClick({ category }, e)}>{category}</button>
+        )
+      }
+      )}</div>
+      <h3>{jokes.joke}</h3>
     </div>
   );
 }
